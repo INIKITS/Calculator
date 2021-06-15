c2f05ffdff0da782067ad7c5869fa9daf73c32c7
Dakota Solis <dakota.h.solis@gmail.com>
2021-06-14 19:22:20 -0500

    New project from TOP to make a functioning calculator with a GUI.

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a project from TOP to create a functioning calculator with a GUI.
---
+++ calc.js
@@ -0,0 +1,24 @@
+function sum(x,y){
+    return x+y;
+}
+
+function subtract(x,y){
+    return x-y;
+}
+
+function product(x,y){
+    return x*y;
+}
+
+function divide(x,y){
+    return x/y;
+}
+
+function operate(x,y,oper){
+    switch(oper){
+        case '+': sum(x,y);
+        case '-': subtract(x,y);
+        case '*': product(x,y);
+        case '/': divide(x,y);
+    }
+}
